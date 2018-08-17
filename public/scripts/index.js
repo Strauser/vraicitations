cef41562299f24b6d3ab9c971f80e97d5b646698
Strauser <e.strauser@gmail.com>
2018-08-17 11:47:27 +0200

    test http REST get

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,3 +1,10 @@
+fetch('https://jsonplaceholder.typicode.com/todos', {
+        method: 'POST',
+        body:JSON.stringify({title:"a new todo"})
+}).then((res) => res.json())
+.then((data) =>  console.log(data))
+.catch((err)=>console.error(err))
+
 var app = new Vue({
   el: '#app',
   data: {
