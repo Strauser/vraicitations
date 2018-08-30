68f5fb90fd7fb987af5197a54b6ddd850f4af356
Strauser <e.strauser@gmail.com>
2018-08-30 17:34:03 +0200

    Update V2__Fill_quotes.sql

--- conf/db/migration/default/V2__Fill_quotes.sql
+++ conf/db/migration/default/V2__Fill_quotes.sql
@@ -1,5 +1,8 @@
 INSERT INTO authors(name) VALUES ("Jacques Chirac");
 INSERT INTO authors(name) VALUES ("Ghandi");
 
-INSERT INTO quotes(quote, truequote, author) VALUES ("Ta mère dab", "Ta mère aime le sport", 1);
-INSERT INTO quotes(quote, truequote, author) VALUES ("La loi du plus fort est toujours la meilleure", "La loi du plus fort est toujours la meilleure", 2);
+INSERT INTO contents(content, type, person, tense) VALUES ("dab", "verb", "first", "present");
+INSERT INTO contents(content, type, person, tense) VALUES ("dans ton cul", "space", NULL, NULL);
+
+INSERT INTO quotes(quote, truequote, content, author) VALUES ("Ta mère [...]", "Ta mère aime le sport", 1, 1);
+INSERT INTO quotes(quote, truequote, content, author) VALUES ("La loi du plus fort est [...]", "La loi du plus fort est toujours la meilleure", 2, 2);
