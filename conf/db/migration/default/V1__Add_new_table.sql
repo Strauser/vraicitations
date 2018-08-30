9864e33581975d891c26863c2e1c1806970c4a9e
Strauser <e.strauser@gmail.com>
2018-08-30 17:33:51 +0200

    Update V1__Add_new_table.sql

--- conf/db/migration/default/V1__Add_new_table.sql
+++ conf/db/migration/default/V1__Add_new_table.sql
@@ -5,12 +5,24 @@ CREATE TABLE IF NOT EXISTS authors (
     PRIMARY KEY (author_id)
 );
 
+CREATE TABLE IF NOT EXISTS contents (
+    content_id INT AUTO_INCREMENT NOT NULL,
+    content VARCHAR(2000),
+    type VARCHAR(47) NOT NULL,
+    person VARCHAR(47),
+    tense VARCHAR(47),
+
+    PRIMARY KEY (content_id)
+);
+
 CREATE TABLE IF NOT EXISTS quotes (
     quote_id INT AUTO_INCREMENT NOT NULL,
-    quote VARCHAR(2000),
+    quote VARCHAR(2000) NOT NULL,
+    content INT NOT NULL,
     truequote VARCHAR(2000),
     author INT NOT NULL,
 
     PRIMARY KEY (quote_id),
-    FOREIGN KEY (author) REFERENCES authors(author_id) ON DELETE CASCADE
+    FOREIGN KEY (author) REFERENCES authors(author_id) ON DELETE CASCADE,
+    FOREIGN KEY (content) REFERENCES contents(content_id)
 );
