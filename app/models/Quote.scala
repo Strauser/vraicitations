5281bfd903d89f94244210ac6fa8861db6b22a2b
Strauser <e.strauser@gmail.com>
2018-08-30 17:28:21 +0200

    Update Quote.scala

--- app/models/Quote.scala
+++ app/models/Quote.scala
@@ -7,15 +7,22 @@ import play.api.libs.json._
 case class Quote(
   quoteId: Int,
   quote: String,
+  content: QuoteContent,
   author: Author
 ) {
-
-
-
+  def getfullQuote: Quote =
+    Quote(
+      quoteId,
+      quote.replace(Quote.REPLACE_IDENTIFIER, content.content),
+      content,
+      author
+  )
 }
 
 object Quote {
-  implicit val reader: Reads[Quote] = Json.reads[Quote]
+  val REPLACE_IDENTIFIER = "[...]"
+
+  implicit val reader: Reads[Quote]   = Json.reads[Quote]
   implicit val writer: OWrites[Quote] = Json.writes[Quote]
   implicit val format: OFormat[Quote] = Json.format[Quote]
 
@@ -23,7 +30,9 @@ object Quote {
     Quote(
       rs.getInt("quote_id"),
       rs.getString("quote"),
+      QuoteContent.fromRow(rs),
       Author.fromRow(rs)
     )
   }
+
 }
