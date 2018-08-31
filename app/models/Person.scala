26d4bc680999b8da1bf3e3767c73b4e8ecce73e8
Strauser <e.strauser@gmail.com>
2018-08-31 17:20:46 +0200

    Update Person.scala

--- app/models/Person.scala
+++ app/models/Person.scala
@@ -2,7 +2,7 @@ package models
 
 import play.api.libs.json.{Json, OFormat, OWrites, Reads}
 
-case class Person(name: String) {}
+case class Person(id: Int, name: String) {}
 
 object Person {
 
@@ -10,17 +10,17 @@ object Person {
   implicit val writer: OWrites[Person] = Json.writes[Person]
   implicit val format: OFormat[Person] = Json.format[Person]
 
-  val First        = Person("first")
-  val Second       = Person("second")
-  val Third        = Person("third")
-  val FirstPlural  = Person("firstPlural")
-  val SecondPlural = Person("secondPlural")
-  val ThridPlural  = Person("thridPlural")
+  val FIRST         = Person(1, "Première personne du singulier")
+  val SECOND        = Person(2, "Seconde personne du singulier")
+  val THIRD         = Person(3, "Troisième personne du singulier")
+  val FIRST_PLURAL  = Person(4, "Première personne du pluriel")
+  val SECOND_PLURAL = Person(5, "Seconde personne du pluriel")
+  val THIRD_PLURAL  = Person(6, "Troisième personne du pluriel")
 
-  val persons: List[Person] = List(First, Second, Third, FirstPlural, SecondPlural, ThridPlural)
+  val persons: List[Person] = List(FIRST, SECOND, THIRD, FIRST_PLURAL, SECOND_PLURAL, THIRD_PLURAL)
 
-  def parse(name: String): Person = {
-    persons.filter((person: Person) => person.name.equals(name)).head
+  def parse(id: Int): Person = {
+    persons.filter((person: Person) => person.id.equals(id)).head
   }
 
 }
