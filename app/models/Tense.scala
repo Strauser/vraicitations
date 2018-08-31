5e19119142a8d02596ed3c2130480c56138e4695
Strauser <e.strauser@gmail.com>
2018-08-31 17:21:22 +0200

    Update Tense.scala

--- app/models/Tense.scala
+++ app/models/Tense.scala
@@ -2,7 +2,7 @@ package models
 
 import play.api.libs.json.{Json, OFormat, OWrites, Reads}
 
-case class Tense(name: String) {}
+case class Tense(id: Int, name: String) {}
 
 object Tense {
 
@@ -10,15 +10,15 @@ object Tense {
   implicit val writer: OWrites[Tense] = Json.writes[Tense]
   implicit val format: OFormat[Tense] = Json.format[Tense]
 
-  val Present     = Tense("present")
-  val Past        = Tense("past")
-  val Futur       = Tense("futur")
-  val PastPresent = Tense("pastPresent")
+  val Present     = Tense(1, "Présent")
+  val Past        = Tense(2, "Passé")
+  val Futur       = Tense(3, "Futur")
+  val PastPresent = Tense(4, "Passé composé")
 
   val tenses: List[Tense] = List(Present, Past, Futur, PastPresent)
 
-  def parse(name: String): Tense = {
-    tenses.filter(tense => tense.name.equals(name)).head
+  def parse(id: Int): Tense = {
+    tenses.filter(tense => tense.id.equals(id)).head
   }
 
 }
