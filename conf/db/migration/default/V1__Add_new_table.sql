e6b53f0158c3dec663bf89f041f3443cfaab086f
Strauser <e.strauser@gmail.com>
2018-08-31 17:23:36 +0200

    Update V1__Add_new_table.sql

--- conf/db/migration/default/V1__Add_new_table.sql
+++ conf/db/migration/default/V1__Add_new_table.sql
@@ -8,9 +8,9 @@ CREATE TABLE IF NOT EXISTS authors (
 CREATE TABLE IF NOT EXISTS contents (
     content_id INT AUTO_INCREMENT NOT NULL,
     content VARCHAR(2000),
-    type VARCHAR(47) NOT NULL,
-    person VARCHAR(47),
-    tense VARCHAR(47),
+    type INT NOT NULL,
+    person INT,
+    tense INT,
 
     PRIMARY KEY (content_id)
 );
@@ -18,7 +18,9 @@ CREATE TABLE IF NOT EXISTS contents (
 CREATE TABLE IF NOT EXISTS quotes (
     quote_id INT AUTO_INCREMENT NOT NULL,
     quote VARCHAR(2000) NOT NULL,
-    content INT NOT NULL,
+    type INT NOT NULL,
+    person INT,
+    tense INT,
     truequote VARCHAR(2000),
     author INT NOT NULL,
 
